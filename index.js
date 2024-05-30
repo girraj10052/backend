fcbf32f1164093708a17d39c7484d88b49de67b9
Girraj Bihari Gupta <148784145+girraj10052@users.noreply.github.com>
2024-05-30 22:31:54 +0530

    update link frontend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 
 const app = express() 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://employee-management-using-react.vercel.app/"],
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true
 }))
